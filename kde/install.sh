f25622b26362c5eca3d7e84b156ebe171bb6a3c0
Mark N Carpenter Jr <mcarpenter@smtcorp.com>
2021-11-30 13:21:01 -0500

    REFACTOR: Remove COLORD Session block

--- kde/install.sh
+++ kde/install.sh
@@ -104,16 +104,6 @@ if [ ! -e /etc/modules-load.d/hv_sock.conf ]; then
   echo "hv_sock" > /etc/modules-load.d/hv_sock.conf
 fi
 
-# Configure the policy xrdp session
-cat > /etc/polkit-1/localauthority/50-local.d/45-allow-colord.pkla <<EOF
-[Allow Colord all Users]
-Identity=unix-user:*
-Action=org.freedesktop.color-manager.create-device;org.freedesktop.color-manager.create-profile;org.freedesktop.color-manager.delete-device;org.freedesktop.color-manager.delete-profile;org.freedesktop.color-manager.modify-device;org.freedesktop.color-manager.modify-profile
-ResultAny=no
-ResultInactive=no
-ResultActive=yes
-EOF
-
 # reconfigure the service
 systemctl daemon-reload
 systemctl start xrdp
